268a81288d137bfa08d1e070107627ea29d46903
James Randolph <james.randolph2016@gmail.com>
2018-03-29 14:44:10 +0000

    added functionality to search by name or numbe

--- lib/flatiron_courses/course.rb
+++ lib/flatiron_courses/course.rb
@@ -11,3 +11,13 @@ class FlatironCourses::Course
     @@all ||= scrape_flatiron_courses
   end
   
+  def self.find(id)
+    self.all[id-1]
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect do |c|
+      c.name.downcase.strip == name.downcase.strip || c.name.split("(").first.strip.downcase == name.downcase.strip
+    end
+  end
+  

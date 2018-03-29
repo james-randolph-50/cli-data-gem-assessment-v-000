b5c6f71dd23381581a6e015ef0b9f20abfdf61c9
James Randolph <james.randolph2016@gmail.com>
2018-03-29 14:39:30 +0000

    Added course.rb with attr_acccessors, init method, and self.all

---
+++ lib/flatiron_courses/course.rb
@@ -0,0 +1,13 @@
+class FlatironCourses::Course
+  
+  attr_accessor :name, :duration, :summary
+  
+  def initialize(name=nil, duration=nil)
+    @name = name
+    @duration = duration
+  end
+  
+  def self.all
+    @@all ||= scrape_flatiron_courses
+  end
+  

63d5a667bfe2c2fad52bbef32973fd46073ae73a
James Randolph <james.randolph2016@gmail.com>
2018-03-29 23:10:44 +0000

    filling in the divs that I copied from the dev console, need to add '.' in order to concatenate the css selectors.

--- lib/flatiron_courses/course.rb
+++ lib/flatiron_courses/course.rb
@@ -41,8 +41,9 @@ class FlatironCourses::Course
             course = self.new
             course.name = new_course.search("div.heading.heading--level-2.heading--level-2--no-flex").text.strip
             course.duration = new_course.search("h6.heading.heading--level-6.heading--level-6--color-grey-dark.util__margin-ntxs").text.strip
-            binding.pry
+            
             course.summary = new_course.search("div.text-block p").text.strip
+            binding.pry
     
       course
     end

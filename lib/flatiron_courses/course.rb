58cccf348cd6d8b97fad72d3d1070f27688f16dc
James Randolph <james.randolph2016@gmail.com>
2018-03-29 22:54:20 +0000

    more stuff

--- lib/flatiron_courses/course.rb
+++ lib/flatiron_courses/course.rb
@@ -34,12 +34,13 @@ class FlatironCourses::Course
     doc = Nokogiri::HTML(open("https://flatironschool.com/our-courses/"))
     
     
-    courses = doc.search("ul.list.list--separators.list--separators--top.list--separators--color-grey-faint.list--spacing-flex-large")
-      binding.pry
+    courses = doc.search("ul.list.list--separators.list--separators--top.list--separators--color-grey-faint.list--spacing-flex-large li")
+      
   #  if courses.doc.search("a.button.button--color-blue-inverted.button--size-small") do
        courses.each do |new_course|
             new_course = self.new
-            course.name = new_course.search("div.heading heading--level-2 heading--level-2--no-flex").text.strip
+            new_course.name = courses.search("div.heading.heading--level-2.heading--level-2--no-flex").text.strip
+            binding.pry
             course.duration = new_course.search("h6.heading heading--level-6 heading--level-6--color-grey-dark util__margin-ntxs").text.strip
             course.summary = new_course.search("div.text-block p").text.strip
     

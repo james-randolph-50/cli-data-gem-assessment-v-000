8aacb013894acdea24d9e19b7c90213f71d2d5b8
James Randolph <james.randolph2016@gmail.com>
2018-03-29 23:08:50 +0000

    Copying divs from inspect drops the '.' that concatenates the css selectors, adding them now.

--- lib/flatiron_courses/course.rb
+++ lib/flatiron_courses/course.rb
@@ -40,8 +40,8 @@ class FlatironCourses::Course
        courses.each do |new_course|
             course = self.new
             course.name = new_course.search("div.heading.heading--level-2.heading--level-2--no-flex").text.strip
+            course.duration = new_course.search("h6.heading.heading--level-6.heading--level-6--color-grey-dark.util__margin-ntxs").text.strip
             binding.pry
-            course.duration = new_course.search("h6.heading heading--level-6 heading--level-6--color-grey-dark util__margin-ntxs").text.strip
             course.summary = new_course.search("div.text-block p").text.strip
     
       course

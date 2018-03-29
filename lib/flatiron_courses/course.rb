59ff4657b9100ee880cd47f7c068b3742ef0f1cb
James Randolph <james.randolph2016@gmail.com>
2018-03-29 23:26:10 +0000

    commit

--- lib/flatiron_courses/course.rb
+++ lib/flatiron_courses/course.rb
@@ -36,16 +36,17 @@ class FlatironCourses::Course
     
     courses = doc.search("ul.list.list--separators.list--separators--top.list--separators--color-grey-faint.list--spacing-flex-large li")
       
-  #  if courses.doc.search("a.button.button--color-blue-inverted.button--size-small") do
+
        courses.each do |new_course|
             course = self.new
             course.name = new_course.search("div.heading.heading--level-2.heading--level-2--no-flex").text.strip
             course.duration = new_course.search("h6.heading.heading--level-6.heading--level-6--color-grey-dark.util__margin-ntxs").text.strip
             
             course.summary = new_course.search("div.text-block p").text.strip
-            binding.pry
+            
     
       course
+      binding.pry
     end
     
   end

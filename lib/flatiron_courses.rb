f1ac5ac86ff88c49e2620a163520e76b5ce503b3
James Randolph <james.randolph2016@gmail.com>
2018-03-29 14:18:15 +0000

    Added libfolder

---
+++ lib/flatiron_courses.rb
@@ -0,0 +1,10 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+require_relative "flatiron-courses/version"
+require_relative "flatiron-courses/cli"
+require_relative "flatiron-courses/course"
+
+module Flatiron_Courses
+end
